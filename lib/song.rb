c17cd9f5409ad4756490bb7c31578dc2d83f85c5
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-17 20:37:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,13 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.each do |song|
+    all.each do |song|
       return song if song.name == name
     end
     false
   end
 
   def self.find_or_create_by_name(name)
-    
+    if find_by_name
   end
 end
